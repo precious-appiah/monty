1992edd41de06031fc9e1ed396e6a6a5ec235775
precious-appiah <appiahprecious99@gmail.com>
2023-11-18 21:31:53 -0800

    main file

--- main.c
+++ main.c
@@ -1,6 +1,28 @@
-#include "main.h"
+#include "monty.h"
 /**
  * main- entry for code
- * Return: nothing
+ * @argc: argc count
+ * @argv: array
+ * Return: int
  */
-void main(void);
+int main(int argc, char *argv[])
+{
+	FILE *file;
+
+	if (argc != 2)
+	{
+		exit(EXIT_FAILURE);
+	}
+	
+	file = fopen(argv[1], "r");
+
+	if(!file)
+	{
+		exit(EXIT_FAILURE);
+	}
+
+	execute_file(file);
+	fclose(file);
+
+	return (0);
+}
